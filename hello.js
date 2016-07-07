f915f54554ef03960d20fa6880df5d0dc59bb8fa
Diego Andres Cardenas <diego.cardenas@ingenieros.com>
2016-07-07 12:11:48 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://script.google.com/macros/s/AKfycbzghJLvbFvaJopeB2_hBW4lzVT60LzVC2xdIOmxlV1WwiqiikI/exec"
     }).then(function(data) {
-       $('.exec-nombre').append(data.id);
-       $('.exec-sueldo').append(data.content);
+       $('.nombre').append(data.id);
+       $('.sueldo').append(data.content);
     });
 });
